82f91d361c1e366475e7328f1d3a177c67a8d827
A. F. M Shahriar Sharif (Rahi) <afmssrahi@gmail.com>
2023-02-20 01:53:55 +0600

    Update index.tsx

--- packages/client/src/pages/Dashboard/index.tsx
+++ packages/client/src/pages/Dashboard/index.tsx
@@ -1,49 +1,11 @@
-import { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import ImageAvatar from '../../../../material-ui/src/Atoms/Avatars/ImageAvatar';
-import Cont from '../../../../material-ui/src/Atoms/Container';
-import Copyright from '../../../../material-ui/src/Atoms/Copyright';
-import PrimaryButton from '../../../../material-ui/src/Atoms/PrimaryButton';
-import Text from '../../../../material-ui/src/Atoms/Text';
-import SectionHead from '../../../../material-ui/src/Molecules/SectionHead';
-import NavBar from '../../../../material-ui/src/Organisms/NavBar/NavBar';
-import photo from '../../assets/avatar.jpg';
+import React from 'react'
 
-type Props = {};
+type Props = {}
 
-const Dashboard = ({ }: Props) => {
-    const [count, setCount] = useState(0);
-    const {
-        control,
-        handleSubmit,
-        formState: { errors },
-    } = useForm({
-        defaultValues: {
-            username: '',
-            email: '',
-            phone: '',
-            password: '',
-            confirmPassword: '',
-        },
-    });
-
+const Dashboard = (props: Props) => {
     return (
-        <Cont>
-            <SectionHead title='Skills' />
+        <div>Dashboard</div>
+    )
+}
 
-            <NavBar />
-
-
-            <ImageAvatar Src={photo} Txt='Name' Size={150} />
-            <div>Heoolo</div>
-            <PrimaryButton />
-            <Text />
-
-
-
-            <Copyright />
-        </Cont>
-    );
-};
-
-export default Dashboard;
+export default Dashboard
