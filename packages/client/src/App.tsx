e9dca4136ed6ddcdb135971ff7d128befc281a47
A. F. M Shahriar Sharif (Rahi) <afmssrahi@gmail.com>
2023-02-19 21:04:50 +0600

    :bug: fix: react router dom

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,52 +1,20 @@
-import { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import ImageAvatar from '../../material-ui/src/Atoms/Avatars/ImageAvatar';
-import Cont from '../../material-ui/src/Atoms/Container/index';
-import Copyright from '../../material-ui/src/Atoms/Copyright/index';
-import FormContaineer from '../../material-ui/src/Atoms/FormContainer/index';
-import Input from '../../material-ui/src/Atoms/InputG/index';
-import PrimaryButton from '../../material-ui/src/Atoms/PrimaryButton/index';
-import SectionBody from '../../material-ui/src/Atoms/SectionBody/index';
-import Text from '../../material-ui/src/Atoms/Text';
-import SectionHead from '../../material-ui/src/Molecules/SectionHead/index';
-import NavBar from '../../material-ui/src/Organisms/NavBar/NavBar';
-import photo from './assets/avatar.jpg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { router } from './routes';
 function App() {
-	const [count, setCount] = useState(0);
-	const {
-		control,
-		handleSubmit,
-		formState: { errors },
-	} = useForm({
-		defaultValues: {
-			username: '',
-			email: '',
-			phone: '',
-			password: '',
-			confirmPassword: '',
-		},
-	});
 	return (
-		<Cont>
-			<SectionHead title='Skills' />
-
-			<NavBar />
-
-			<SectionBody>
-				<ImageAvatar Src={photo} Txt='Name' Size={150} />
-				<div>Heoolo</div>
-				<PrimaryButton />
-				<Text />
-				<FormContaineer>
-					<Input
-						item={{ name: 'username', type: 'text' }}
-						control={control}
-					/>
-				</FormContaineer>
-			</SectionBody>
-
-			<Copyright />
-		</Cont>
+		<Router>
+			<Routes>
+				{router.map((route, index) => {
+					return (
+						<Route
+							key={index}
+							path={route.path}
+							element={<route.component />}
+						/>
+					);
+				})}
+			</Routes>
+		</Router>
 	);
 }
 
