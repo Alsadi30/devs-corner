32e46399ae876904514b601e00838d5d95fcade4
A. F. M Shahriar Sharif (Rahi) <afmssrahi@gmail.com>
2023-02-21 14:33:43 +0600

    :construction: :sparkles: feat: authentication and (private, public) route

--- packages/client/src/pages/Register/index.tsx
+++ packages/client/src/pages/Register/index.tsx
@@ -1,102 +1,128 @@
-import { SubmitHandler, useForm } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup';
-import { registerSchema } from "../../utils/validation/AuthValidation";
+import { Button, Grid, Typography } from '@mui/material';
+import { useEffect } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom';
+import FormContainer from '../../../../material-ui/src/Atoms/FormContainer';
+import Input from '../../../../material-ui/src/Atoms/InputG';
 import MapListItem from '../../../../material-ui/src/Atoms/MapListItem/index';
-import Input from "../../../../material-ui/src/Atoms/InputG";
-import { Typography, Grid, Button } from '@mui/material';
-import FormContainer from "../../../../material-ui/src/Atoms/FormContainer";
 import Layout from '../../../../material-ui/src/Organisms/Layout/index';
+import { useRegisterMutation } from '../../features/auth/authApi';
+import { registerSchema } from '../../utils/validation/AuthValidation';
 
 export interface RegisterFormInput {
-    username: string;
-    email: string;
-    phone: string;
-    password: string;
-    confirmPassword?: string;
+	username: string;
+	email: string;
+	phone: string;
+	password: string;
+	confirmPassword?: string;
 }
 
-
 let inputs = [
-    {
-        name: 'username',
-        type: 'text',
-    },
-    {
-        name: 'email',
-        type: 'text',
-    },
-    {
-        name: 'phone',
-        type: 'text',
-
-    },
-    {
-        name: 'password',
-        type: 'password',
-
-    },
-    {
-        name: 'confirmPassword',
-        type: 'password',
-    }
-]
+	{
+		name: 'username',
+		type: 'text',
+	},
+	{
+		name: 'email',
+		type: 'text',
+	},
+	{
+		name: 'phone',
+		type: 'text',
+	},
+	{
+		name: 'password',
+		type: 'password',
+	},
+	{
+		name: 'confirmPassword',
+		type: 'password',
+	},
+];
 
 const Register = () => {
+	const [register, { data, isLoading, error: responseError }] =
+		useRegisterMutation();
 
-    //use of react hook from with validation by yup
-    const { control, handleSubmit, formState: { errors }, } = useForm({
-        defaultValues: {
-            username: '',
-            email: '',
-            phone: '',
-            password: '',
-            confirmPassword: ''
-        }, resolver: yupResolver(registerSchema)
-    });
+	const navigate = useNavigate();
 
+	useEffect(() => {
+		if (responseError?.data) {
+			console.log(responseError.data);
+		}
+		if (data?.accessToken && data?.user) {
+			navigate('/dashboard');
+		}
+	}, [data, responseError, navigate]);
 
-    const onSubmit: SubmitHandler<RegisterFormInput> = async data => {
-        if (data.password === data.confirmPassword) {
+	//use of react hook from with validation by yup
+	const {
+		control,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		defaultValues: {
+			username: '',
+			email: '',
+			phone: '',
+			password: '',
+			confirmPassword: '',
+		},
+		resolver: yupResolver(registerSchema),
+	});
 
+	const onSubmit: SubmitHandler<RegisterFormInput> = async (data2) => {
+		if (data2?.password === data2?.confirmPassword) {
+			console.log(data2);
+			register(data2);
+		} else {
+			alert("Password don't match with confirm password");
+		}
+	};
 
+	return (
+		<Layout>
+			<Grid container justifyContent='center' alignItems='center'>
+				<Grid item xs={12} sm={8} md={5} lg={4} xl={4}>
+					<FormContainer handleSubmit={handleSubmit(onSubmit)}>
+						<Typography mb={2} variant='h2' component='h6'>
+							Register
+						</Typography>
 
-        } else {
-            alert("Password don't match with confirm password")
-        }
-    };
+						{/* Input Items map through MapItems */}
+						<MapListItem
+							Items={inputs}
+							Component={Input}
+							other={control}
+						/>
 
-    return (
-        <Layout>
-            <Grid container justifyContent="center"
-                alignItems="center">
-                <Grid item xs={12} sm={8} md={5} lg={4} xl={4} >
-                    <FormContainer
-                        handleSubmit={handleSubmit(onSubmit)}
-                    >
-                        <Typography mb={2} variant="h2" component="h6">
-                            Register
-                        </Typography>
+						<Button
+							variant='contained'
+							fullWidth={true}
+							sx={{
+								margin: '10px 0px',
+								bgcolor: 'primary.main',
+								color: 'white',
+							}}
+							type='submit'
+						>
+							Submit
+						</Button>
 
-                        {/* Input Items map through MapItems */}
-                        <MapListItem Items={inputs} Component={Input} other={control} />
-
-                        <Button
-                            variant="contained"
-                            fullWidth={true}
-                            sx={{
-                                margin: '10px 0px', bgcolor: 'primary.main', color: 'white'
-                            }} type="submit">Submit</Button>
-
-                        <Typography variant="h5" color={'info.main'} component="p">
-                            Already have an account?
-                            Login
-
-                        </Typography>
-                    </FormContainer>
-                </Grid>
-            </Grid>
-        </Layout>
-    )
+						<Typography
+							variant='h5'
+							color={'info.main'}
+							component='p'
+						>
+							Already have an account?{' '}
+							<Link to='/login'>Login</Link>
+						</Typography>
+					</FormContainer>
+				</Grid>
+			</Grid>
+		</Layout>
+	);
 };
 
-export default Register
+export default Register;
