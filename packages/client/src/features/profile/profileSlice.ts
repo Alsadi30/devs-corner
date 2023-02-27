f9cac80dd622cc714a22309642ea0ff09a0823bc
A. F. M Shahriar Sharif (Rahi) <afmssrahi@gmail.com>
2023-02-27 19:16:28 +0600

    :construction: feat: profile get and post (in rtk-query) done

---
+++ packages/client/src/features/profile/profileSlice.ts
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {};
+
+const profileSlice = createSlice({
+	name: 'profile',
+	initialState,
+	reducers: {},
+});
+
+export const {} = profileSlice.actions;
+export default profileSlice.reducer;
--- packages/client/src/store/store.ts
+++ packages/client/src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from '../features/api/apiSlice';
 import authSliceReducer from '../features/auth/authSlice';
+import profileSliceReducer from '../features/profile/profileSlice';
 
 export const store = configureStore({
 	reducer: {
 		[apiSlice.reducerPath]: apiSlice.reducer,
 		auth: authSliceReducer,
+		profile: profileSliceReducer,
 	},
 	devTools: process.env.NODE_ENV !== 'production',
 	middleware: (getDefaultMiddlewares) =>
