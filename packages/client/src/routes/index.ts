9afcb29f76a17ac001b598d5fe0a4048af8a60c2
MD. Ariful Islam <41154962+arif-btkrm@users.noreply.github.com>
2023-02-28 16:52:46 +0600

    Created Resume page and included in route

--- packages/client/src/routes/index.ts
+++ packages/client/src/routes/index.ts
@@ -1,32 +1,38 @@
-import Dashboard from '../pages/DashBoard/index';
-import Login from '../pages/Login';
-import NotFound from '../pages/NotFound';
-import Register from '../pages/Register/index';
+import Dashboard from "../pages/DashBoard";
+import Login from "../pages/Login";
+import NotFound from "../pages/NotFound";
+import Register from "../pages/Register/index";
+import Resume from "../pages/Resume/index";
 
 export const router = [
-	{
-		path: '/',
-		component: Dashboard,
-		public: false,
-	},
-	{
-		path: '/dashboard',
-		component: Dashboard,
-		public: false,
-	},
-	{
-		path: '/login',
-		component: Login,
-		public: true,
-	},
-	{
-		path: '/register',
-		component: Register,
-		public: true,
-	},
-	{
-		path: '*',
-		component: NotFound,
-		public: true,
-	},
+  {
+    path: "/",
+    component: Dashboard,
+    public: false,
+  },
+  {
+    path: "/dashboard",
+    component: Dashboard,
+    public: false,
+  },
+  {
+    path: "/login",
+    component: Login,
+    public: true,
+  },
+  {
+    path: "/register",
+    component: Register,
+    public: true,
+  },
+  {
+    path: "/resume/:id",
+    component: Resume,
+    public: true,
+  },
+  {
+    path: "*",
+    component: NotFound,
+    public: true,
+  },
 ];
